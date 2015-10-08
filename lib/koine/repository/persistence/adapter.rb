05a0a2e2c406b93dd990547e0a3bf095e23c79e0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-10-08 18:26:38 +0200

    Fixed broken test

--- lib/koine/repository/persistence/adapter.rb
+++ lib/koine/repository/persistence/adapter.rb
@@ -2,31 +2,31 @@ module Koine
   module Repository
     module Persistence
       class Adapter
-        def exists?(criterias)
+        def exists?(*args)
           raise "Method not implemented: 'exists?'"
         end
 
-        def find_one_by(criterias)
+        def find_one_by(*args)
           raise "Method not implemented: 'find_one_by'"
         end
 
-        def update_where(criterias, values)
+        def update_where(*args)
           raise "Method not implemented: 'update_where'"
         end
 
-        def find_all_by(criterias)
+        def find_all_by(*args)
           raise "Method not implemented: 'find_all_by'"
         end
 
-        def find(id)
+        def find(*args)
           raise "Method not implemented: 'find'"
         end
 
-        def insert(values)
+        def insert(*args)
           raise "Method not implemented: 'insert'"
         end
 
-        def delete_where(criterias)
+        def delete_where(*args)
           raise "Method not implemented: 'delete_where'"
         end
       end

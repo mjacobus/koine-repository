14bc5cd95593dee1231a7ee089234b2b5e4522e9
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-10-08 14:14:20 +0200

    Added coverage tasks

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'koine/repository'
 
+
+if ENV['COVERALLS']
+  require 'coveralls'
+  Coveralls.wear!
+end
+
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter "/test/"
+    add_group "Repository", "lib/koine/repository/repository"
+    add_group "Persistency", "lib/koine/repository/persistence"
+  end
+end
+
+require 'koine/repository'
 require 'minitest/autorun'
